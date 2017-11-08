52e9b1e32249480bce56b024c1538a6d6a435de9
Malcolm Wigren <trekommafem2@gmail.com>
2017-11-08 16:02:48 +0100

    Did some more

--- TSRT78/lesson3.m
+++ TSRT78/lesson3.m
@@ -29,6 +29,23 @@ stem((2*pi/N)*[0:N-1], abs(X)); title('X');
 subplot(2, 1, 2);
 stem((2*pi/Ndec/Tdec)*[0:Ndec-1], abs(XDEC)); title('X_{dec}');
 
-% a. We get aliasing as a result of decreasing the number of samples.
+% a. We get aliasing, since the resulting frequency is less than half the sampling
+% frequency
 
+%% 3.2
+
+load('sig30');
+
+N = length(y);
+
+figure(1);
+subplot(2, 2, 1);
+plot(y); title('Original signal y');
+
+Y = fft(y);
+subplot(2, 2, 2);
+stem(2*pi/N*[0:N-1], abs(Y)); title('DTF\{Y\}')
+
+N2 = 2*N;
+% TODO Zero pad
 
